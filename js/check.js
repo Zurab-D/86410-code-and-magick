39707d5af37428157f44194f84651ca79bdfaef7
Zurab-D <dzn@inbox.com>
2016-01-21 00:43:48 +0300

    module1-task2 исправил замечания: 1. убрал проверку !! когда параметр булевский, 2. вынес в отдельные функции циклы сложения/умножения элементов массивов

--- js/check.js
+++ js/check.js
@@ -1,15 +1,35 @@
-function getMessage(a, b) {
+/* проверка является строка числом */
+function isNumber (strIn) {
+  return (typeof(strIn) === "number") && isFinite(strIn);
+};
+
+/* сумма элементов массива */
+function sumArrElements(arr) {
+  var result = 0;
+  for (var i=0; i<arr.length; i++) {
+    result += arr[i];
+  };
+  return result;
+};
+
+/* сумма произведений соответствующих элементов двух массивов */
+function multiple2ArrElements(arr1, arr2) {
+  var result = 0;
+  for (var i=0; i<arr1.length && i < arr2.length; i++) {
+    result += arr1[i] * arr2[i];
+  };
+  return result;
+};
+
+/* решение задачи 1.5 */
+function getMessage (a, b) {
   if (typeof(a) === "boolean") {
-    return !!a ? "Я попал в " + b : "Я никуда не попал";
-  } else if (typeof(a) === "number") {
+    return a ? "Я попал в " + b : "Я никуда не попал";
+  } else if (isNumber(a)) {
     return "Я прыгнул на " + a * 100 + " сантиметров";
   } else if (Array.isArray(a) && Array.isArray(b)) {
-    var length = 0;
-    for (var i=0; i<a.length; i++) length += a[i] * b[i];
-    return "Я прошёл " + length + " метров"
+    return "Я прошёл " + multiple2ArrElements(a, b) + " метров"
   } else if (Array.isArray(a)) {
-    var sum = 0;
-    for (var i=0; i<a.length; i++) sum += a[i];
-    return "Я прошёл " + sum + " шагов";
+    return "Я прошёл " + sumArrElements(a) + " шагов";
   }
-}
+};

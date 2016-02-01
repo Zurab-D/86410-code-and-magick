fa971886ce8e7fbef924d728b1aedb23fbb21be8
Zurab-D <dzn@inbox.com>
2016-02-01 21:10:01 +0300

    мусор подчистил

--- js/reviews.js
+++ js/reviews.js
@@ -19,17 +19,12 @@
 
   /* выводим элементы на страницу */
   function renderNewElements(review) {
-    //console.log(review);
-    //document.writeln(review.description + '<br>');
     if ('content' in reviewTemplate) {
       var clone = reviewTemplate.content.children[0].cloneNode(true);
     } else {
       var clone = reviewTemplate.children[0].cloneNode(true);
     }
-    //var cloneImg = clone.querySelector('.review-author');
 
-    //review.date
-    //review.review_usefulness
     clone.querySelector('.review-rating').style.width = (30 * review.rating) + 'px';
     clone.querySelector('.review-text').textContent = review.description;
 
@@ -48,22 +43,19 @@
     /* при загрузке картинки назначим ее путь в атрибут SRC */
     img.onload = function() {
       clearTimeout(timeout);
-      img.src = review.author.picture; /* img.src; /* вариант с полным путем */
+      img.src = review.author.picture;
     }
 
     /* при ошибке загрузке картинки в атрибут SRC пишем картинку ошибки загрузки */
     img.onerror = function(err) {
-      console.log('err: '+img.alt);
+      //console.log('err: '+img.alt); - если раскомментиовать, видно, что onerror постоянно срабатывает
       errorLoadImage();
-      //img.src = '';
-      //clone.classList.add('review-load-failure--onerror');
     }
 
     /* взводим таймаут, по которому назначим картинку ошибки загрузки */
     var TIMEOUT_IMAGE = 10000; /* 10 сек. */
     var timeout = setTimeout(function() {
       errorLoadImage();
-      //clone.classList.add('review-load-failure--timeout');
     }, TIMEOUT_IMAGE)
 
     /* начинаем загрузку картинки */
@@ -71,8 +63,6 @@
 
     clone.replaceChild(img, clone.querySelector('.review-author'));
     reviewsList.appendChild(clone);
-
-    /* ВОПРОС: а объект img удалять в конце не нужно ??? */
   }
 
 
